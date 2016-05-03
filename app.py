1a62d3ed912e9e0e10f36ebfda38d9c5d22aab5f
mq <markus.quade@gmail.com>
2016-05-04 00:24:57 +0200

    register form done

--- app.py
+++ app.py
@@ -1,20 +1,43 @@
 import web
 from web import form
 
+import model
+
+DB_NAME = "db.db"
+
 render = web.template.render('templates') # your templates
 
 vpass = form.regexp(r".{3,20}$", 'must be between 3 and 20 characters')
 vemail = form.regexp(r".*@.*", "must be a valid email address")
 
-register_form = form.Form(
-    form.Textbox("username", description="Username"),
-    form.Textbox("email", vemail, description="E-Mail"),
-    form.Password("password", vpass, description="Password"),
-    form.Password("password2", description="Repeat password"),
-    form.Button("submit", type="submit", description="Register"),
-    validators = [
-        form.Validator("Passwords did't match", lambda i: i.password == i.password2)]
+def check_bot(i):
+    return i["botcheck"] in ("Abel", "Feudel", "Grassberger", "Politi", "Rosenblum")
 
+botmsg = "Please enter any last name of an organiser."
+
+def check_submission(i, key):
+    if i["ptype"] == "None":
+        return True
+    else:
+        return bool(i[key])
+
+register_form = form.Form(
+    form.Dropdown ("title", ('Mr.', 'Mrs.', 'Dr.', 'Prof.'), description="Title"),
+    form.Textbox("surname", form.notnull, description="Surname"),
+    form.Textbox("name", form.notnull, description="Name"),
+    form.Textbox("institut", form.notnull, description="Institute"),
+    form.Textbox("city", form.notnull, description="City"),
+    form.Textbox("country", form.notnull, description="Country"),
+    form.Textbox("email", vemail, description="E-Mail"),
+    form.Radio("ptype", ("Poster", "None"), form.notnull, description="Type"),
+    form.Textarea("ptitle", description="Title", cols=70, rows=1),
+    form.Textarea("pabstract", description="Abstract", cols=70, rows=20),
+    form.Textarea("pcomment", description="Comment", cols=70, rows=3),
+    form.Textbox("botcheck", value=botmsg, description="Botcheck", size=len(botmsg)),
+    form.Button("submit", type="submit", description="Register"),
+    validators = [form.Validator("Poster title is missing.", lambda i: check_submission(i, "ptitle")),
+                  form.Validator("Poster abstract is missing.", lambda i: check_submission(i, "abstract")),
+                  form.Validator("Botcheck failed.", check_bot)]
 )
 
 class register:
@@ -28,8 +51,10 @@ class register:
         if not f.validates():
             return render.register(f)
         else:
-            pass
-            # do whatever is required for registration
+            with model.make_connection(DB_NAME) as connection:
+                model.dump_registration(f.value, connection)
+            return web.seeother("/")
+
 
 urls = ("/", register)
 
--- make_test_db.sql
+++ make_test_db.sql
@@ -1,3 +1,3 @@
-create table registration(title, surname, name, institut, city, country, email, ptitle, pabstract, ptype, pcomment);
+create table registration(title, surname, name, institute, city, country, email, ptitle, pabstract, ptype, pcomment);
 
 insert into registration values("Mr.", "Max", "Mustermann", "None", "Berlin", "Germany", "a@bc.de", "", "", "None", "");
