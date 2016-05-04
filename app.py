cf8ff2399f5e21d37d6a901b7a6235eb5be58ad5
mq <mquade@uni-potsdam.de>
2016-05-04 11:37:57 +0200

    pep8tyfy

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 import web
 from web import form
 
-import model
-
 DB_NAME = "db.db"
 DB_TABLE = "registration"
 DB_COLUMNS = ("title", 'surname', 'name', 'institute', 'city', 'country', 'email', 'ptitle', 'pabstract', 'ptype', 'pcomment')
 
 db = web.db.database(dbn='sqlite', db=DB_NAME)
 
-render = web.template.render('templates') # your templates
-
+render = web.template.render('templates')
 vpass = form.regexp(r".{3,20}$", 'must be between 3 and 20 characters')
 vemail = form.regexp(r".*@.*", "must be a valid email address")
+botmsg = "Please enter any last name of an organizer."
+
 
 def check_bot(i):
     return i["botcheck"] in ("Abel", "Feudel", "Grassberger", "Politi", "Rosenblum")
 
-botmsg = "Please enter any last name of an organizer."
 
 def check_submission(i, key):
     if i["ptype"] == "None":
@@ -25,8 +23,9 @@ def check_submission(i, key):
     else:
         return bool(i[key])
 
+
 register_form = form.Form(
-    form.Dropdown ("title", ('Mr.', 'Mrs.', 'Dr.', 'Prof.'), description="Title"),
+    form.Dropdown("title", ('Mr.', 'Mrs.', 'Dr.', 'Prof.'), description="Title"),
     form.Textbox("surname", form.notnull, description="Surname"),
     form.Textbox("name", form.notnull, description="Name"),
     form.Textbox("institute", form.notnull, description="Institute"),
@@ -39,14 +38,14 @@ register_form = form.Form(
     form.Textarea("pcomment", description="Comment", cols=70, rows=3),
     form.Textbox("botcheck", value=botmsg, description="Botcheck", size=len(botmsg)),
     form.Button("submit", type="submit", description="Register"),
-    validators = [form.Validator("Poster title is missing.", lambda i: check_submission(i, "ptitle")),
-                  form.Validator("Poster abstract is missing.", lambda i: check_submission(i, "abstract")),
-                  form.Validator("Botcheck failed.", check_bot)]
+    validators=[form.Validator("Poster title is missing.", lambda i: check_submission(i, "ptitle")),
+                form.Validator("Poster abstract is missing.", lambda i: check_submission(i, "abstract")),
+                form.Validator("Botcheck failed.", check_bot)]
 )
 
-class register:
+
+class Register:
     def GET(self):
-        # do $:f.render() in the template
         f = register_form()
         return render.register(f)
 
@@ -61,7 +60,7 @@ class register:
             return web.seeother("/")
 
 
-urls = ("/", register)
+urls = ("/", Register)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
