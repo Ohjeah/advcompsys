bc2694190b90593f1969f30569eb0b58946d26b8
mq <mquade@uni-potsdam.de>
2016-05-12 13:39:35 +0200

    db script

---
+++ make_db.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sqlite3 db.db < make_test_db.sql
--- make_test_db.sql
+++ make_test_db.sql
@@ -1,3 +1,1 @@
 create table registration(title, surname, name, institute, city, country, email, ptitle, pabstract, ptype, pcomment);
-
-insert into registration values("Mr.", "Max", "Mustermann", "None", "Berlin", "Germany", "a@bc.de", "", "", "None", "");
