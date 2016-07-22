4ea95e081df2c4201bb56265622675539b2e70ab
mq <mquade@uni-potsdam.de>
2016-07-22 10:12:10 +0200

    db_to_csv

--- db_to_csv.py
+++ db_to_csv.py
@@ -5,10 +5,10 @@ from app import DB_NAME, DB_TABLE
 
 
 if __name__ == "__main__":
-    query = "FROM {} SELECT *;".format(DB_TABLE)
+    query = "FROM {0} SELECT *;".format(DB_TABLE)
     with sqlite3.connect(DB_NAME) as con:
         result = con.cursor().execute(query)
 
-    with open("{}.csv".format(DB_TABLE), 'w') as f:
+    with open("{0}.csv".format(DB_TABLE), 'w') as f:
         writer = csv.writer(f)
         writer.writerows(result)
