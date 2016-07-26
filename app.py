acbe620cf1a23b288d65ce3d94a7519a66deddf4
mq <mquade@uni-potsdam.de>
2016-07-26 11:35:10 +0200

    123

--- app.py
+++ app.py
@@ -152,7 +152,9 @@ patterns = re.compile("[Tt]est"), re.compile("[Qq]uade")
 
 class Participants:
     def GET(self):
+
         if web.ctx.env[authkey] is not None:
+            return web.ctx.env[authkey]
             result = db.select(DB_TABLE)
             result = set(map(tuple, map(order, result)))  # filter duplicates, parse entry
             result = map(encode, filter(no_test_row, result)) # remove tests and encode to utf
